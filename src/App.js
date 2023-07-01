a3d8e7647b724ea03926349377be25351c2b3861
DGarg375 <gargdivyansh11@gmail.com>
2023-06-30 18:39:15 -0700

    add background-animation

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import BackgroundAnimation from './components/backgroundanimation/BackgroundAnimation';
 
 function App() {
   return (
     <div className="App">
-      
+      <div class="background-animation">
+        <BackgroundAnimation width={window.innerWidth} height={window.innerHeight} startframe="0" endframe="212"></BackgroundAnimation>
+      </div>
     </div>
   );
 }
---
+++ src/components/backgroundanimation/BackgroundAnimation.js
@@ -0,0 +1,68 @@
+import React, { useEffect, useRef } from 'react';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import './BackgroundAnimation.css';
+
+gsap.registerPlugin(ScrollTrigger);
+
+const images = [];
+
+const BackgroundAnimation = props => {
+    const canvasRef = useRef(null);
+    const endFrame = props.endframe;
+    let ball = { frame: 0 };
+
+    const currentFrame = (index) => `./frames/Frame (${(index+1).toString()}).jpg`;
+    useEffect(() => {
+        let render;
+        // Get canvas and 2d context
+        const canvas = canvasRef.current;
+        const context = canvas.getContext('2d');
+
+        //Get and create all images
+        for(let i = 0; i < endFrame; i++) {
+            const img = new Image();
+            img.src = currentFrame(i);
+            images.push(img);
+        }
+
+        let tl = gsap.timeline();
+        if(canvas.width < 530) {
+            render = () => {
+                context.canvas.width = images[ball.frame].width;
+                context.canvas.height = images[ball.frame].height;
+                context.clearRect(0, 0, canvas.width, canvas.height);
+                context.fillStyle = "black";
+                context.fillRect(0, 0, canvas.width, canvas.height);
+            }
+        } else {
+            render = () => {
+                const currentImage = images[ball.frame];
+                context.canvas.width = images[ball.frame].width;
+                context.canvas.height = images[ball.frame].height;
+                context.clearRect(0, 0, canvas.width, canvas.height);
+                context.drawImage(currentImage, 0, 0);
+            };
+        }
+
+        tl.to(ball, {
+            frame: endFrame-1,
+            snap: "frame",
+            ease: "none",
+            scrollTrigger: {
+                scrub: true,
+                pin: "canvas",
+                end: '500%',
+            },
+            onUpdate: render,
+        });
+
+        images[ball.frame].onload = render;
+
+    }, []);
+    return (
+        <canvas ref={canvasRef} {...props}></canvas>
+    );
+}
+
+export default BackgroundAnimation;
