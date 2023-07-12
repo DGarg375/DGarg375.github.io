f857047f26f53eb257e931bc699907533ab1d0ad
DGarg375 <gargdivyansh11@gmail.com>
2023-07-12 14:14:48 -0700

    minor fixes

--- src/aboutMeta.js
+++ src/aboutMeta.js
@@ -8,19 +8,19 @@ export const aboutBundle = {
         "useComponent": "divyansh",
     },
     "slide_2": {
-        "imgSrc": "./icons8-dev-windows-11/davis.png",
+        "imgSrc": "./icons8-dev-windows-11/icons8-ucdavis.jpeg",
         "imgAlt": "education",
         "title": "Education",
         "useComponent": "educt",
     },
     "slide_3": {
-        "imgSrc": "./icons8-dev-windows-11/skills3.png",
+        "imgSrc": "./icons8-dev-windows-11/icons8-skills.png",
         "imgAlt": "skills",
         "title": "Skills",
         "useComponent": "skill",
     },
     "slide_4": {
-        "imgSrc": "./icons8-dev-windows-11/cert.png",
+        "imgSrc": "./icons8-dev-windows-11/icons8-certificate.jpg",
         "imgAlt": "certifications",
         "title": "Certifications",
         "useComponent": "certif",
